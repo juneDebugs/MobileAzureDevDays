6a334adb26a8a34c7f11f468c737107a232eaff8
June Cho <junecho.ucb@gmail.com>
2017-09-28 14:13:55 -0700

    Initial Commit

---
+++ MobileAzureDevDays/Pages/HomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MobileAzureDevDays.Pages
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
