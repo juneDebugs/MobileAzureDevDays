341bbb71e5a117fd7746b6845c289205dc7b79d1
June Cho <junecho.ucb@gmail.com>
2017-09-28 17:23:45 -0700

    Added CodeBehind for HomePage

--- Xamarin/MobileAzureDevDays/Pages/HomePage.xaml.cs
+++ Xamarin/MobileAzureDevDays/Pages/HomePage.xaml.cs
@@ -5,11 +5,33 @@ using Xamarin.Forms;
 
 namespace MobileAzureDevDays.Pages
 {
-    public partial class HomePage : ContentPage
-    {
-        public HomePage()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class HomePage : ContentPage
+	{
+		//readonly HomeViewModel viewModel;
+
+		public HomePage()
+		{
+			InitializeComponent();
+
+			//viewModel = new HomeViewModel();
+			//BindingContext = viewModel;
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			//viewModel.SentimentAnalyisFailed += HandleSentimentAnalyisFailed;
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+
+			//viewModel.SentimentAnalyisFailed -= HandleSentimentAnalyisFailed;
+		}
+
+		void HandleSentimentAnalyisFailed(object sender, string ErrorMessage) =>
+			Device.BeginInvokeOnMainThread(async () => await DisplayAlert("Error", ErrorMessage, "OK"));
+	}
 }
