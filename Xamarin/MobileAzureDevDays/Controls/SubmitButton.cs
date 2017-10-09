d7cd9e0fa5b47845fddd4dabe16d17a2f5d6a37c
brminnick <brandon.r.minnick@gmail.com>
2017-10-09 14:28:41 -0700

    Formatted Submit Button

---
+++ Xamarin/MobileAzureDevDays/Controls/SubmitButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MobileAzureDevDays.Controls
+{
+    public class SubmitButton : Button
+    {
+        public SubmitButton()
+        {
+            BackgroundColor = Color.Transparent;
+            BorderColor = Color.White;
+            BorderRadius = 10;
+            BorderWidth = 2;
+        }
+    }
+}
+
