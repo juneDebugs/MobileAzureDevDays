dc791274ad760d93b5a70600285137a6f3c142f5
brminnick <brandon.r.minnick@gmail.com>
2018-02-14 17:14:20 -0800

    Refactored to use Page Object Architecture

---
+++ Swift/MobileAzureDevDays/MobileAzureDevDaysUITests/Tests/MobileAzureDevDaysUITests.swift
@@ -0,0 +1,46 @@
+//
+//  MobileAzureDevDaysUITests.swift
+//  MobileAzureDevDaysUITests
+//
+//  Edited by Brandon Minnick on Feb 02 2018
+//  Created by Sweekriti Satpathy on 11 October 2017.
+//  Copyright © 2017 Colby Williams. All rights reserved.
+//
+
+import XCTest
+import AppCenterXCUITestExtensions
+
+class MobileAzureDevDaysUITests: XCTestCase {
+    
+    let app = XCUIApplication()
+    
+    var sentimentPage: SentimentPage!
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        
+        ACTLaunch.launch(app)
+        sentimentPage = SentimentPage(app: app)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func happyTest() {
+        //Arrange
+        let happyText = "Happy"
+        
+        //Act
+        sentimentPage.enterText(text: happyText)
+        sentimentPage.tapSubmitButton()
+        sentimentPage.waitForActivityIndicator(timeout: 10)
+        sentimentPage.waitForNoActivityIndicator(timeout: 10)
+        
+        //Assert
+        let result = sentimentPage.getResults()
+        XCTAssertTrue(result == "😃")        
+        ACTLabel.labelStep("Correct Result Present")
+    }
+}
